d324a4aab83db4fec0bae5b91e09feb5556f003a
Ugyen Jigten <ujigten@gmail.com>
2017-02-03 15:44:32 +0700

    Added Views and Activity for every category.

--- app/src/main/java/com/example/android/autochilddetectorappcompanion/MainActivity.java
+++ app/src/main/java/com/example/android/autochilddetectorappcompanion/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.autochilddetectorappcompanion;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView setup = (TextView) findViewById(R.id.setupView);
+        TextView guide = (TextView) findViewById(R.id.guideView);
+        TextView kidscars = (TextView) findViewById(R.id.kidscarView);
+
+        setup.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent setupIntent = new Intent(MainActivity.this, SetupActivity.class);
+                startActivity(setupIntent);
+            }
+        });
+
+        guide.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent guideIntent = new Intent(MainActivity.this, GuideActivity.class);
+                startActivity(guideIntent);
+            }
+        });
+
+        kidscars.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent kidcarsIntent = new Intent(MainActivity.this, KidsCarsActivity.class);
+                startActivity(kidcarsIntent);
+            }
+        });
     }
 }

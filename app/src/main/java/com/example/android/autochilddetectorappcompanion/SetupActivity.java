80b03012b555ea5341207d17c2276fee366705e7
Ugyen Jigten <ujigten@gmail.com>
2017-02-05 15:52:25 +0700

    Added SMS functionality and Setup view

--- app/src/main/java/com/example/android/autochilddetectorappcompanion/SetupActivity.java
+++ app/src/main/java/com/example/android/autochilddetectorappcompanion/SetupActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.autochilddetectorappcompanion;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class SetupActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class SetupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+    }
+
+    public void setup(View view)    {
+        EditText arduinoNumber = (EditText) findViewById(R.id.arNum);
+        String arNum = arduinoNumber.getText().toString();
+        EditText personalNumber = (EditText) findViewById(R.id.uNum);
+        String urNum = personalNumber.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("smsto:" + arNum));
+        intent.putExtra("sms_body", urNum);
+        startActivity(intent);
     }
 }
